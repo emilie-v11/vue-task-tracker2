03e98c297284b6aa58ca51e4a36c7e158e153c4f
Vukovic Emilie <emilievukovic79@gmail.com>
2025-07-22 14:31:01 +0200

    add all project

---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tasks" v-if="!tasks.length">
+    <p>No tasks to show</p>
+  </div>
+  <div :key="task.id" v-else v-for="task in tasks">
+    <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+};
+</script>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
